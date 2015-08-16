089d9ea0e05925842a41be062f2f20cc2fc97083
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-16 18:08:53 -0400

    init app

--- add-app.js
+++ add-app.js
@@ -1,6 +1,6 @@
 angular.module('AddApp', ['Calc'])
   .controller('AddController', function ($scope, add) {
-    $scope.a = $scope.b = 0;
+    $scope.a = $scope.b = $scope.sum = 0;
 
     $scope.$watch('a', function () {
       $scope.sum = add($scope.a, $scope.b);
